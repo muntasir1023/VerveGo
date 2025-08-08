d23828e3eb498f8c3a5a200f55ce5bfb2cf527ef
muntasir1023 <143379955+muntasir1023@users.noreply.github.com>
2025-08-09 00:00:30 +0600

    Add files via upload

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(mongodb+srv://muntasir1023:<Nirjhor1023>@cluster0.os3ed8l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
